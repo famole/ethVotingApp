3f2d0a958b583fda215382d866bb465103ffa1c8
Fabian Molina <famole@gmail.com>
2019-07-11 21:09:44 -0300

    Cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,16 +6,32 @@ contract Election {
     string name;
     uint voteCount;
   }
+  //Store accounts that have voted
+  mapping(address => bool) public voters;
 
   mapping(uint => Candidate) public candidates;
+
   uint public candidatesCount;
   constructor () public {
     addCandidate("Candidate 1");
     addCandidate("Candidate 2");
   }
 
-  function addCandidate (string _name) private {
+  function addCandidate (string memory _name) private {
     candidatesCount ++;
-    candidates[candidatesCount] = Candidates(candidatesCount, _name, 0);
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint _candidateId) public {
+    //Require that they haven't voted before
+    require(!voters[msg.sender], "");
+    
+    //Require a valid address
+  require(_candidateId > 0 && _candidateId <= candidatesCount, "");
+    //Record that voter has voted
+    voters[msg.sender] = true;
+
+    //Update candidate vote count
+    candidates[_candidateId].voteCount ++;
   }
 }
